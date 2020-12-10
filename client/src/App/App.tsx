e6f15dad859058f3bc6d49bbc93a164cbd911bd1
Gilbert <gilwong3105@gmail.com>
2020-12-10 01:34:11 -0800

    Re init client

---
+++ client/src/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar } from 'Navbar';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
--- client/src/UI/index.ts
+++
@@ -1,4 +0,0 @@
-export { Button, ButtonLabel } from './Button';
-export { default as Loading } from './Loading';
-export { default as AddPhotoButton } from './AddPhotoButton';
-export { default as FileUpload } from './FileUpload';
